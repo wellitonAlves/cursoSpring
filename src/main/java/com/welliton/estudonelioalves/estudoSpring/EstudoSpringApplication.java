9b6a6cd6983fb63823f2591efa5df7828ba56e26
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-05 23:00:39 -0300

    Projeto criado

---
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/EstudoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.welliton.estudonelioalves.estudoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/welliton/estudonelioalves/estudoSpring/EstudoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.welliton.estudonelioalves.estudoSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstudoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
