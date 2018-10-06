faa47a4095cf896b0c29f0704b46b4d3e2b7e99d
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-06 16:34:16 -0300

    instaciando banco na inicializacao

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/EstudoSpringApplication.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/EstudoSpringApplication.java
@@ -1,12 +1,30 @@
 package com.welliton.estudonelioalves.estudoSpring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.welliton.estudonelioalves.estudoSpring.domain.Categoria;
+import com.welliton.estudonelioalves.estudoSpring.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class EstudoSpringApplication {
+public class EstudoSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 =  new Categoria (null, "Informatica");
+		Categoria cat2 =  new Categoria (null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
