73d8060a50e1529ddcdf502b8c434db9181e53b2
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-13 00:15:28 -0300

    paginacao

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/services/CategoriaService.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/services/CategoriaService.java
@@ -5,14 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.welliton.estudonelioalves.estudoSpring.domain.Categoria;
 import com.welliton.estudonelioalves.estudoSpring.repository.CategoriaRepository;
 import com.welliton.estudonelioalves.estudoSpring.services.exception.DataIntegrityException;
 import com.welliton.estudonelioalves.estudoSpring.services.exception.ObjectNotFoundException;
-
-import net.bytebuddy.implementation.bytecode.Throw;
 
 @Service
 public class CategoriaService {
@@ -53,6 +54,13 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy,String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 	
 }
