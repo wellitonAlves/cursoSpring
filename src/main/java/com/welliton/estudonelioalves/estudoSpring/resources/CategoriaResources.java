0b78c20f0585999cc6a0b3c8b19cb7907f25e7df
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-12 21:25:03 -0300

    Atualizando categoria com put

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/resources/CategoriaResources.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/resources/CategoriaResources.java
@@ -25,7 +25,7 @@ public class CategoriaResources {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> listar(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj); 
 		
 	}
@@ -39,4 +39,13 @@ public class CategoriaResources {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj =  service.update(obj);
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.noContent().build();
+	}
 }
