4871990a1fb80d2ebff7f8cec9d7541fec616e64
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-13 21:18:24 -0300

    personalizando erro

---
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/resources/exception/FieldMessage.java
@@ -0,0 +1,39 @@
+package com.welliton.estudonelioalves.estudoSpring.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldNome;
+	private String message;
+	
+	private FieldMessage() {
+		
+	}
+
+	public FieldMessage(String fieldNome, String message) {
+		super();
+		this.fieldNome = fieldNome;
+		this.message = message;
+	}
+
+	public String getFieldNome() {
+		return fieldNome;
+	}
+
+	public void setFieldNome(String fieldNome) {
+		this.fieldNome = fieldNome;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
