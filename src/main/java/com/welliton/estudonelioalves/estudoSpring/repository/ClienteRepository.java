3c2ab9c6c16663a63c67993eb2bc789121cd3dbf
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-14 21:18:26 -0300

    validacao customizada email não reptido no cadastro de cliente

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/repository/ClienteRepository.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/repository/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.welliton.estudonelioalves.estudoSpring.repository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.welliton.estudonelioalves.estudoSpring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/welliton/estudonelioalves/estudoSpring/resources/validation/ClienteInsertValidator.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/resources/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.welliton.estudonelioalves.estudoSpring.domain.Cliente;
 import com.welliton.estudonelioalves.estudoSpring.domain.enums.TipoCliente;
 import com.welliton.estudonelioalves.estudoSpring.dto.ClienteNewDTO;
+import com.welliton.estudonelioalves.estudoSpring.repository.ClienteRepository;
 import com.welliton.estudonelioalves.estudoSpring.resources.exception.FieldMessage;
 import com.welliton.estudonelioalves.estudoSpring.resources.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "email já cadastrado"));
 		}
 		
 
