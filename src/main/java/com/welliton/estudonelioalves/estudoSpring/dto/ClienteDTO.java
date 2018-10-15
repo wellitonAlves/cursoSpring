c40036fc36ef8306f21620fd2c8cafcc3412f993
JOSE WELLITON <josealves_w@yahoo.com.br>
2018-10-14 21:32:14 -0300

    validacao customizada email não repetido na atualizcao de cliente

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/dto/ClienteDTO.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.welliton.estudonelioalves.estudoSpring.domain.Cliente;
+import com.welliton.estudonelioalves.estudoSpring.resources.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	
 	
