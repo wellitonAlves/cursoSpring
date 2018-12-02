77ce448103692d593aca187fe3ee381e351fe594
JOSÉ WELLITON ALVES <josealves_w@yahoo.com.br>
2018-12-02 00:12:59 -0200

    implementando autorizacao

--- src/main/java/com/welliton/estudonelioalves/estudoSpring/config/SecurityConfig.java
+++ src/main/java/com/welliton/estudonelioalves/estudoSpring/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.welliton.estudonelioalves.estudoSpring.security.JWTAuthenticationFilter;
+import com.welliton.estudonelioalves.estudoSpring.security.JWTAuthorizationFilter;
 import com.welliton.estudonelioalves.estudoSpring.security.JWTUtil;
 
 @Configuration
@@ -55,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
